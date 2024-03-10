341af542ceeae0f7719ad92bb471e66a555fa28e
Mustakim Islam Alif <alifisalu@gmail.com>
2024-03-10 19:19:10 +0600

    Initial commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,25 +10,25 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Colors.blue[900],
       body: Column(
         children: [
-          Text('Hello, welcome back!'),
-          Text('Login to continue'),
-          TextField(
+          const Text('Hello, welcome back!'),
+          const Text('Login to continue'),
+          const TextField(
             decoration: InputDecoration(hintText: 'Username'),
           ),
-          TextField(
+          const TextField(
             decoration: InputDecoration(hintText: 'Password'),
           ),
           TextButton(
               onPressed: () {
                 print('Forgot is Clicked');
               },
-              child: Text('Forgot Password?')),
+              child: const Text('Forgot Password?')),
           ElevatedButton(
               onPressed: () {
                 print('Forgot is Clicked');
               },
-              child: Text('Log in')),
-          Text('Or sign in with'),
+              child: const Text('Log in')),
+          const Text('Or sign in with'),
           ElevatedButton(
             onPressed: () {
               print('Google is Clicked');
@@ -40,7 +40,7 @@ class LoginPage extends StatelessWidget {
                   width: 22,
                   height: 22,
                 ),
-                Text('Login with Google'),
+                const Text('Login with Google'),
               ],
             ),
           ),
@@ -55,16 +55,16 @@ class LoginPage extends StatelessWidget {
                   width: 22,
                   height: 22,
                 ),
-                Text('Login with Facebook'),
+                const Text('Login with Facebook'),
               ],
             ),
           ),
           Row(
             children: [
-              Text('Don\'t have an account?'),
+              const Text('Don\'t have an account?'),
               TextButton(
                 onPressed: () {},
-                child: Text('Sign up'),
+                child: const Text('Sign up'),
               ),
             ],
           ),
