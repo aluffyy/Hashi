d59ca64997d3ff201714e85fd432d8dc614d7538
Mustakim Islam Alif <alifisalu@gmail.com>
2024-03-18 03:26:26 +0600

    Initial commit

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
